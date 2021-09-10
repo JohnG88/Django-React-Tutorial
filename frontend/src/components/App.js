f8ce862a919afb3687f7bae354b1b569affdfe1a
John Gutierrez <djterez88@gmail.com>
2021-09-10 12:26:03 -0700

    Added some basic simple react class components and rendered them in App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
+import HomePage from "./HomePage"
+import RoomJoinPage from "./RoomJoinPage"
+import CreateRoomPage from "./CreateRoomPage"
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+        // can add this.state, which is same as useState
+        // if state is changed/updated then it will rerender that component
+        // this.state = {}
     }
 
     render() {
-        return <h1>Testing React Code</h1>
+        return (
+            <div>
+                <HomePage />
+                <RoomJoinPage/>
+                <CreateRoomPage />
+            </div>)
     }
 }
 
---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        // can add this.state, which is same as useState
+        // if state is changed/updated then it will rerender that component
+        // this.state = {}
+    }
+
+    render() {
+        return <p>This is the create room join page</p>
+    }
+}
